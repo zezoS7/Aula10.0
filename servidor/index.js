c02a6ecee820a7d744f8753dc65aa98656b23cb8
zezoS7 <zezoedu.f15@hotmail.com>
2024-11-14 12:36:43 -0300

    Aula10.0.1

--- servidor/index.js
+++ servidor/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const app = express ();
 const PORT = 3001;
 
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: 'mysqlfatec',
     database: 'aulabd'
 });
 
@@ -24,3 +24,37 @@ db.connect((erro) => {
         }
     });
 
+// Rota para cadastrar usuário 
+app.post('/alunos', (req, res) => {
+    const { nome, cidade, estado } = req.body;
+
+    const sql = 'INSERT INTO alunos (nome, cidade, estado) VALUES (?, ?, ?)';
+
+    db.query(sql, {nome, cidade, estado}, (err, result) => {
+        if (err)
+        {
+            return res.status(500).json({ error: 'Erro ao cadastrar aluno! '});
+        }
+        res.status(201).json({message: 'Aluno cadastrado com sucesso!', id: result.insertId });
+
+    });
+
+});
+
+// Iniciando o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+// Rota para consultar todos os usuários
+app.get('/alunos', (req, res) => {
+    const sql = 'SELECT * FROM alunos';
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Erro ao consultar alunos' });
+
+        }
+        res.json(results);
+        
+    });
+});
